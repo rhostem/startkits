66b4febabddc3b33d9237ebd80665c94e573722c
rhostem <syoung.j@gmail.com>
2021-02-03 16:51:13 +0900

    setup jest

--- react-snowpack-ts/src/App.test.tsx
+++ react-snowpack-ts/src/App.test.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
-import { expect } from 'chai';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('<App>', () => {
   it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
+    render(<App />);
+    expect(screen.getByText(/learn react/i)).toBeInTheDocument();
   });
 });
