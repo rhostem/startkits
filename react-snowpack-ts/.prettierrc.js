2ae7afe7f1fd120fe4e5d157dcb57f2899901089
rhostem <syoung.j@gmail.com>
2021-02-04 11:47:01 +0900

    setup plugin:prettier/recommended

--- react-snowpack-ts/.prettierrc.js
+++ react-snowpack-ts/.prettierrc.js
@@ -1,12 +1,27 @@
 module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
+  quoteProps: 'as-needed',
   trailingComma: 'all',
+  bracketSpacing: true,
   jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  parser: 'typescript',
   overrides: [
     {
       files: '*.css',
       options: {
         parser: 'css',
+      },
+    },
+    {
+      files: '*.scss',
+      options: {
+        parser: 'scss',
       },
     },
     {
