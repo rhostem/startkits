659e686c2ab98a18b03e9ba504e67e29eebf7bdb
rhostem <syoung.j@gmail.com>
2021-05-16 19:39:41 +0900

    tailwind form

--- react-snowpack-tailwind/src/stories/tailwindcss/Form.stories.tsx
+++ react-snowpack-tailwind/src/stories/tailwindcss/Form.stories.tsx
@@ -4,14 +4,41 @@ export default {
   title: 'tailwindcss/Form',
 };
 
-const Template = () => {
+const InputForm = ({ label }: { label: string }) => {
   return (
-    <div className='p-4'>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam amet
-      obcaecati nesciunt qui consequatur praesentium repudiandae placeat. Totam
-      dolores vero molestias deserunt quo. Nihil dicta sunt quasi amet magni ex!
+    <label>
+      <div className='text-gray-600'>{label}</div>
+      <input
+        type='text'
+        className='w-full transition mt-1 rounded border-gray-300'
+      />
+    </label>
+  );
+};
+
+const Form = () => {
+  return (
+    <div className='p-4 grid grid-cols-1 gap-4 w-1/3'>
+      <InputForm label='Name'></InputForm>
+      <InputForm label='Address'></InputForm>
+      <InputForm label='Age'></InputForm>
+
+      <div className='grid grid-cols-3'>
+        <label className='inline-flex items-center'>
+          <input type='checkbox' className='rounded focus:ring-0' />
+          <span className='pl-2'>Option 1</span>
+        </label>
+        <label className='inline-flex items-center'>
+          <input type='checkbox' className='rounded focus:ring-0' />
+          <span className='pl-2'>Option 2</span>
+        </label>
+        <label className='inline-flex items-center'>
+          <input type='checkbox' className='rounded focus:ring-0' />
+          <span className='pl-2'>Option 3</span>
+        </label>
+      </div>
     </div>
   );
 };
 
-export const Default = Template.bind({});
+export const Default = Form.bind({});
