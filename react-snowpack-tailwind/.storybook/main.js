acd019a99aa4387a45980ce0175ea83fe1f57853
rhostem <syoung.j@gmail.com>
2021-05-16 18:21:39 +0900

    add storybook file

---
+++ react-snowpack-tailwind/.storybook/main.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const webpack = require('webpack');
+const snowpackConfig = require('../snowpack.config');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+  ],
+  webpackFinal: async (config) => {
+    // Assign aliases from snowpack.config.js
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...snowpackConfig.alias,
+    };
+
+    // Add __SNOWPACK_ENV__ global
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SNOWPACK_ENV__: JSON.stringify(
+          Object.entries(process.env).reduce(
+            (filtered, [key, value]) =>
+              /^SNOWPACK_PUBLIC_.+/.test(key)
+                ? { ...filtered, [key]: value }
+                : filtered,
+            {},
+          ),
+        ),
+      }),
+    );
+
+    // Add rules for supporting import.meta
+    config.module.rules.push({
+      test: /\.[tj]sx?$/,
+      loader: [
+        require.resolve('@snowpack/plugin-webpack/plugins/import-meta-fix'),
+        require.resolve(
+          '@snowpack/plugin-webpack/plugins/proxy-import-resolve',
+        ),
+      ],
+    });
+
+    // config.module.rules.push({
+    //   test: /\.css$/,
+    //   use: [
+    //     'style-loader',
+    //     'css-loader',
+    //     // {
+    //     //   loader: 'postcss-loader',
+    //     //   options: {
+    //     //     ident: 'postcss',
+    //     //     plugins: [require('tailwindcss'), require('autoprefixer')],
+    //     //   },
+    //     // },
+    //   ],
+    //   include: path.resolve(__dirname, '../'),
+    // });
+
+    return config;
+  },
+};
---
+++ react-snowpack-tailwind/src/stories/tailwindcss/Form.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default {
+  title: 'tailwindcss/Form',
+};
+
+const Template = () => {
+  return (
+    <div className='block'>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam amet
+      obcaecati nesciunt qui consequatur praesentium repudiandae placeat. Totam
+      dolores vero molestias deserunt quo. Nihil dicta sunt quasi amet magni ex!
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
